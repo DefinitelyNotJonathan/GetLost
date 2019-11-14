8b1446db1335560d9d5e112d9f44cb9852712b25
Jonathan <jonretchless@gmail.com>
2019-11-14 09:30:24 -0800

    final

--- index.js
+++ index.js
@@ -40,7 +40,6 @@ function getParks() {
     .then(function(response) {
       $('#park_results').html('');
       $('#search-button').attr('disabled', false);
-      /* Add a check to prevent 'Cannot find blah blah of undefined' errors */
       if(!response || !response.hasOwnProperty('data') || !response.data.hasOwnProperty('length')){
         throw new Error('Returned data has no length!');
       }
@@ -117,7 +116,6 @@ function getWeather(target_item, index){
     })
     .then ( json_resp => {
 
-      /* Add a check to prevent 'Cannot find blah blah of undefined' errors */
       if(!json_resp || !json_resp.hasOwnProperty('Days') || !json_resp.Days.hasOwnProperty('length')){
         throw new Error('No forecast days!');
       }
@@ -133,12 +131,8 @@ function getWeather(target_item, index){
 }
 
 function displayWeather(resp, item, index){
-
-  // console.log(resp);
-  // console.log(item);
   let forecast = resp['Days'];
-
-  if(!forecast || !forecast.hasOwnProperty('length') || !forecast.length){ // false, '', null, undefined, 0
+  if(!forecast || !forecast.hasOwnProperty('length') || !forecast.length){
     console.log('could not cypher forecast!');
     return;
   }
@@ -168,8 +162,7 @@ function displayWeather(resp, item, index){
   <p>Designation: ${item.accessibility.classifications}</p>
   <p>Description: ${item.description}</p>
   <p>Weather Info: ${item.weatheroverview}</p>
-  <p>Directions: ${item.directionsUrl}</p>
-  <p>Website: ${item.reservationsurl}</p>
+  <p>Directions: <a href="${item.directionsUrl}">External Link</a></p>
   <h4>7-Day Forecast</h4>
   <div class="row forecast-row">${forecast_html}</div>
   </div>`;
@@ -180,7 +173,7 @@ function displayWeather(resp, item, index){
   $('#results').removeClass('hidden');
 }
 
-//function to format query format query parameters
+//function to format query parameters
 
 function formatQueryParams(params) {
   const queryItems = Object.keys(params)
@@ -196,95 +189,3 @@ $(function(){
     getParks();
   });
 })
-
-
-/*
-function displayResults(parkObj, weatherObj) {
-  console.log(parkObj)
-  console.log(weatherObj)
-  $('#results-list').empty();
-  for (let i=0; i <parkObj.data.length; i++){
-    $('#results-list').append(
-      `<li><h3>${parkObj.data[i].name}</h3>
-      <p>State: ${parkObj.data[i].addresses.stateCode}
-      <p>Designation: ${parkObj.data[i].accessibility.classifications}</p>
-      <p>Description: ${parkObj.data[i].description}</p>
-      <p>Weather Info: ${parkObj.data[i].weatherOverview}</p>
-      <p>Forecast: ${weatherObj.Days[i].Timeframes[i].date}</p>
-      <p>${weatherObj.Days[i].Timeframes[i].wx_desc}</p>
-      <p>Directions: ${parkObj.data[i].directionsUrl}</p>
-      <p>Website: ${parkObj.data[i].reservationsUrl}</p>
-      </li>`
-    )};
-  $('#results_2').removeClass('hidden');
-}
-//function to set api 1 params and send GET request
-
-function getNPSResults(query) {
-  const params = {
-    stateCode: query,
-    limit: 5,
-    fields: "addresses",
-    api_key: apiNPS
-  };
-  const queryString1 = formatQueryParams(params)
-  const url1 = urlNPS + '?' + queryString1;
-  console.log(url1);
-  $('#parkObjects > .loader').addClass('loading');
-  fetch(url1)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      throw new Error(response.statusText);
-    })
-    .then(function(parkObj) {
-    // console.log('lets view parkObj:');
-    // console.log(parkObj);
-      $('#parkObjects > .loader').removeClass('loading');
-      $('#park_results').html('');
-
-      for (let i=0; i<parkObj.data.length; i++) {
-        $('#park_results').append('<p>'+parkObj.data[i].name+'</p>');
-        getWeatherResults(parkObj.data[i].addresses[0].postalCode);
-
-      }
-      //pulling the same postal code every time
-    })
-    .catch(err => {
-      console.log('error happened');
-      console.log(err);
-      $('#js-error-message').text(`Something went wrong: ${err.message}`);
-    });
-  }
-
-  //function to set api 2 params and send GET request
-
-function getWeatherResults(parkObj) {
-  const zip = parkObj;
-  const params = {
-    app_id:idWthr,
-    app_key:apiWthr
-  };
-  const queryString2= formatQueryParams(params)
-  const url2= urlWthr + zip + '?' + queryString2;
-  console.log(url2);
-  fetch(url2)
-    .then(response => {
-      // console.log('response:');
-      // console.log(response);
-      if (response.ok) {
-        return response.json();
-      }
-      throw new Error(response.statusText);
-    })
-    .then (weatherObj => {displayResults (parkObj, weatherObj);
-    })
-    .catch(err => {
-      console.log('error reported!');
-      console.log(err);
-      $('#js-error-message').text(`Something went wrong: ${err.message}`);
-    });
-  }
-
-*/
